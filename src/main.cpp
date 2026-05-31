be53eeb341f6060282387859b4ce8495a827d463
Troitskiy-code <mrcheleng87@gmail.com>
2026-05-31 13:10:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,17 @@ void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
-    BST<std::string> tree;
-    makeTree(tree, "src/war_peace.txt");
+	BST<std::string> tree;
+	makeTree(tree, "src/war_peace.txt");
 
-    std::cout << "Tree depth: " << tree.depth() << std::endl;
-    int warFreq = tree.search("war");
-    int peaceFreq = tree.search("peace");
-    std::cout << "'war' frequency: " << warFreq << std::endl;
-    std::cout << "'peace' frequency: " << peaceFreq << std::endl;
-    std::cout << "All frequencies:\n";
-    printFreq(tree);
-    return 0;
+	std::cout << "Tree depth: " << tree.depth() << std::endl;
+
+	int warFreq = tree.search("war");
+	int peaceFreq = tree.search("peace");
+	std::cout << "'war' frequency: " << warFreq << std::endl;
+	std::cout << "'peace' frequency: " << peaceFreq << std::endl;
+
+	std::cout << "All frequencies:\n";
+	printFreq(tree);
+	return 0;
 }
